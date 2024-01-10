c932f5522487a15ade07698db5bad55835e5026b
mainakmukherjee <mainakmukherjee019@gmail.comt>
2024-01-10 15:37:19 +0530

    BrowserStack_POC_JavaOnly Code

---
+++ BrowserStack_POC_JavaOnly/src/com/browserstact/poc/IOSTestCases.java
@@ -0,0 +1,34 @@
+package com.browserstact.poc;
+
+import java.net.MalformedURLException;
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class IOSTestCases extends IOSBase {
+
+
+	@Test
+	public void tc001() throws MalformedURLException, InterruptedException
+	{		
+		driver.findElement(By.xpath("//XCUIElementTypeStaticText[@name='Text']")).click();
+		driver.findElement(By.xpath("//XCUIElementTypeTextField[@name='Text Input']")).sendKeys("Mainak");
+		driver.findElement(By.xpath("(//XCUIElementTypeButton[@name='UI Elements'])[1]")).click();
+		driver.findElement(By.xpath("//XCUIElementTypeStaticText[@name='Alert']")).click();
+		driver.findElement(By.xpath("//XCUIElementTypeButton[@name='OK']")).click();
+		driver.findElement(By.xpath("//XCUIElementTypeImage[@name='nav_webview']")).click();
+		driver.findElement(By.xpath("//XCUIElementTypeButton[@name='Local Testing']")).click();
+		driver.findElement(By.xpath("//XCUIElementTypeButton[@name='UI Elements']")).click();
+		Assert.assertTrue(true);
+	}
+	@Test
+	public void tc002() throws MalformedURLException, InterruptedException
+	{
+		driver.findElement(By.xpath("//XCUIElementTypeImage[@name='nav_webview']")).click();
+		int maxSwipe = 5;
+		for(int i = 1; i<=maxSwipe; i++)
+		{
+			verticalSwipe(2, 2, 0.25);
+		}
+	}
+}
